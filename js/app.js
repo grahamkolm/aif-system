eca28f0986d082f0b51f7dc345d0348bd188100e
grahamkolm <grahamkolm@outlook.com>
2026-03-09 18:30:20 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,17 @@
-console.log('module loaded');
+function startSystem(){
+
+console.log("AIF system starting");
+
+if(typeof updateData === "function"){
+    updateData();
+}
+
+setInterval(()=>{
+    if(typeof updateData === "function"){
+        updateData();
+    }
+},60000);
+
+}
+
+window.onload = startSystem;
