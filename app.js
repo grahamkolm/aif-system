b0fdfea08ed135b2eb2b3860eb05a15dae54846b
grahamkolm <grahamkolm@outlook.com>
2026-03-29 22:08:15 +0200

    Update app.js

--- app.js
+++ app.js
@@ -91,7 +91,7 @@ set("wind", wind.toFixed(1) + " km/h");
 set("cloud", cloud + "%");
                 
 renderDashboard(data);
-})  // ← NO semicolon here
+});  // ← NO semicolon here
 
 .catch(err => {
     console.log("FETCH ERROR:", err);
