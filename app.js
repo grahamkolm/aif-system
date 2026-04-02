39575f40e380b0a1cf644e2f6a8a92d80c39f5fe
grahamkolm <grahamkolm@outlook.com>
2026-04-02 13:21:09 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 // =====================================================
 // 🌍 0. START 
 // =====================================================
+const splashCanvas = document.getElementById("splashCanvas");
+const splashCtx = splashCanvas ? splashCanvas.getContext("2d") : null;
 
 function getMoonPhase() {
 			return "Normal";
