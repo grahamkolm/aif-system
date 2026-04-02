dbdbc0c661354d699a7cc822f4f7c02513c19baf
grahamkolm <grahamkolm@outlook.com>
2026-04-02 14:04:42 +0200

    Update app.js

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const splashCanvas = document.getElementById("splashCanvas");
 const splashCtx = splashCanvas ? splashCanvas.getContext("2d") : null;
 
 function set(id, value) {
-	const el = document.grtElementById(id);
+	const el = document.getElementById(id);
 	if (el) el.innerText = value;
 }
 
