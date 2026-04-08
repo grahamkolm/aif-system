47dba7ab6f63090579fe4656410ed8c430ffcfe6
grahamkolm <grahamkolm@outlook.com>
2026-04-08 14:14:03 +0300

    Update app.js

--- app.js
+++ app.js
@@ -148,7 +148,7 @@ function startApp() {
     setInterval(generateHotspots, 10000);
     setInterval(ripple, 3000);
 
-    setTimeout(fetchWeatherSafe, 2000);
+//    setTimeout(fetchWeatherSafe, 2000);
 }
 
 function resizeCanvas() {
