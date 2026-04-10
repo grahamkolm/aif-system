f341b12e64513738a55472a38e9108e8db45415d
grahamkolm <grahamkolm@outlook.com>
2026-04-10 19:22:54 +0200

    Update app.js

--- app.js
+++ app.js
@@ -148,7 +148,7 @@ function startApp() {
     setTimeout(fetchWeatherSafe, 2000);
     
     setTimeout(() => {
-        setupHold("Gauge", showInsight);
+        setupHold("Gauge", showSPIInsight);
     }, 1500);
 }
 
