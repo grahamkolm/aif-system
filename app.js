5e8fc87fb96b7d46ff0cead27c03bf118c04a6a8
grahamkolm <grahamkolm@outlook.com>
2026-04-13 14:11:01 +0200

    Update app.js

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ let SPI = 50;
 
 let bubbles = [];
 let canvas, ctx;
-let ripples =[];
+let ripples = [];
 let scoutData = {};
 let drops = [];
 let splashCanvas;
@@ -735,16 +735,16 @@ function calculateCONF(SPI, envScore, p, w, c, t) {
     if (isNaN(score)) score = 50;
     return Math.max(40, Math.min(95, Math.round(score)));
     
-    // ================= FINAL =================
+}
+
+ // ================= FINAL =================
     
-    console.log("CONF INPUT:", SPI, envScore, p, w, c, t);
     let confScoreValue = calculateCONF(SPI, envScore, p, w, c, t);
     let confEl = document.getElementById("confScore");
-
     if (confEl) {
         confEl.innerText = confScoreValue + "%";
     }
-    
+
 // 🎯 GET ELEMENTS (ONLY ONCE)
 const spiCircle = document.getElementById("spiCircle");
 const envCircle = document.getElementById("envCircle");
