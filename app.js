3ea1b37b50b87e6aca687969071552f33d6b5a13
grahamkolm <grahamkolm@outlook.com>
2026-04-17 13:09:27 +0200

    Update print statement from 'Hello' to 'Goodbye'

--- app.js
+++ app.js
@@ -4,7 +4,7 @@
 
 if ("serviceWorker" in navigator) {
   window.addEventListener("load", () => {
-//    navigator.serviceWorker.register("/service-worker.js")
+      navigator.serviceWorker.register("/service-worker.js")
       .then(reg => console.log("Service Worker registered:", reg.scope))
       .catch(err => console.error("Service Worker failed:", err));
   });
