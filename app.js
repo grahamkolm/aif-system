c84ed4dd3aa06f27841addb1a2e1c58f50d8c170
grahamkolm <grahamkolm@outlook.com>
2026-04-18 09:20:32 +0200

    Update app.js

--- app.js
+++ app.js
@@ -230,8 +230,7 @@ function showCONFInsight() {
 // =====================================================
 
 document.addEventListener("DOMContentLoaded", () => {
-    setupScoutOptions();
-});
+
     // =============================
     // 🌊 SPLASH INIT
     // =============================
